be6621cbea96dfb6b1be0b84145a0d3f8c812e3b
3pointcurve_ <azambinabdullah@gmail.com>
2020-04-22 15:23:33 +0800

    Update create_mahasiswa.blade.php

--- resources/views/mahasiswa/create_mahasiswa.blade.php
+++ resources/views/mahasiswa/create_mahasiswa.blade.php
@@ -56,29 +56,29 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-2 col-form-label text-md-right">{{ __('Password') }}</label>
+{{--                            <div class="form-group row">--}}
+{{--                                <label for="password" class="col-md-2 col-form-label text-md-right">{{ __('Password') }}</label>--}}
 
-                                <div class="col-md-4">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror " name="password" required autocomplete="new-password">
+{{--                                <div class="col-md-4">--}}
+{{--                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror " name="password" required autocomplete="new-password">--}}
 
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
+{{--                                    @error('password')--}}
+{{--                                    <span class="invalid-feedback" role="alert">--}}
+{{--                                        <strong>{{ $message }}</strong>--}}
+{{--                                    </span>--}}
+{{--                                    @enderror--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
 
-                            <div class="form-group row">
-                                <label for="password-confirm" class="col-md-2 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+{{--                            <div class="form-group row">--}}
+{{--                                <label for="password-confirm" class="col-md-2 col-form-label text-md-right">{{ __('Confirm Password') }}</label>--}}
 
-                                <div class="col-md-4">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                </div>
-                            </div>
+{{--                                <div class="col-md-4">--}}
+{{--                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
 
-                            {{ Form::hidden('role', '10') }}
+{{--                            {{ Form::hidden('role', '10') }}--}}
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-2">
