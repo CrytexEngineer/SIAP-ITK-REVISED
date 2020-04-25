1092743f1ab3cc86564d1427ac80662fd92c477b
CrytexEngineer <aqilmaulana1@gmail.com>
2020-04-25 14:02:44 +0800

    Added Count Presence

---
+++ app/Http/Controllers/Mobile/PresenceController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Http\Controllers\Mobile;
+
+
+use App\Http\Controllers\Controller;
+use http\Env\Request;
+use Illuminate\Support\Facades\DB;
+
+class PresenceController extends Controller
+{
+
+    function count(Request $request){
+        $presenceCount= DB::select();
+
+
+    }
+
+    function index(){
+
+
+
+
+    }
+
+}
--- app/Http/Controllers/RekapitulasiMahasiswaController.php
+++ app/Http/Controllers/RekapitulasiMahasiswaController.php
@@ -20,8 +20,9 @@ class RekapitulasiMahasiswaController extends Controller
     }
 
 
-    public function json(){
-        return Datatables::of($this->getRekapitulasiAll())
+    public function json()
+    {
+        return Datatables::of($this->getRekapitulasiAll(array('max_percentage' => '0.5',)))
 
 //            ->addColumn('action', function ($row) {
 //                $action = '<a href="/Khs/' . $row->KU_ID . '/edit" class="btn btn-primary btn-sm"><i class="fas fa-pencil-alt"></i></a>';
@@ -31,7 +32,6 @@ class RekapitulasiMahasiswaController extends Controller
 //                return $action;
 //            })
             ->make(true);
-
 
 
     }
@@ -49,7 +49,7 @@ class RekapitulasiMahasiswaController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -60,7 +60,7 @@ class RekapitulasiMahasiswaController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -71,7 +71,7 @@ class RekapitulasiMahasiswaController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -82,8 +82,8 @@ class RekapitulasiMahasiswaController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -94,7 +94,7 @@ class RekapitulasiMahasiswaController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
@@ -102,31 +102,57 @@ class RekapitulasiMahasiswaController extends Controller
         //
     }
 
-    function  getRekapitulasiAll($MA_Nrp=0){
-       return DB::select("
-       SELECT class_student.KU_ID,
-        class_student.KU_KE_KR_MK_ID,
-        class_student.KU_KE_Kelas,
-        employees.PE_NamaLengkap,
-        t3.KE_Terisi,
-        students.MA_NamaLengkap,
-        students.MA_NRP_Baru,
-        t3.total AS Jumlah_Pertemuan,
-        COUNT(presences.PR_KU_ID)as Kehadiran,
-        COUNT(presences.PR_KU_ID)/t3.total  as Persentase from class_student
-        LEFT Outer  JOIN presences ON presences.PR_KU_ID=class_student.KU_ID
-        JOIN (SELECT  meetings.PT_KE_ID,
-              classes.*,
-              COUNT(*) as total FROM meetings
-              JOIN classes ON meetings.PT_KE_ID=classes.KE_ID
-              GROUP BY meetings.PT_KE_ID
-         ) as t3  on t3.KE_KR_MK_ID=class_student.KU_KE_KR_MK_ID
-         JOIN(students) ON class_student.KU_MA_Nrp= students.MA_Nrp
-          JOIN (employees)ON employees.PE_Nip=t3.KE_PE_NIPPengajar
-         WHERE t3.KE_Kelas=class_student.KU_KE_Kelas
-        group by class_student.KU_ID
-        ORDER BY class_student.KU_KE_KR_MK_ID, class_student.KU_MA_Nrp
-       ");
+    function getRekapitulasiAll($filters = array())
+    {
+        $defaultQuery = "SELECT class_student.KU_ID,
+            class_student.KU_KE_KR_MK_ID,
+            class_student.KU_KE_Kelas,
+            employees.PE_NamaLengkap,
+            t3.KE_Terisi,
+            students.MA_NamaLengkap,
+            students.MA_NRP_Baru,
+            students.MA_Nrp,
+            t3.total AS Jumlah_Pertemuan,
+            COUNT(presences.PR_KU_ID)as Kehadiran,
+            COUNT(presences.PR_KU_ID)/t3.total  as persentase from class_student
+            LEFT Outer  JOIN presences ON presences.PR_KU_ID=class_student.KU_ID
+            JOIN (SELECT  meetings.PT_KE_ID,
+                  classes.*,
+                  COUNT(*) as total FROM meetings
+                  JOIN classes ON meetings.PT_KE_ID=classes.KE_ID
+                  GROUP BY meetings.PT_KE_ID
+             ) as t3  on t3.KE_KR_MK_ID=class_student.KU_KE_KR_MK_ID
+             JOIN(students) ON class_student.KU_MA_Nrp= students.MA_Nrp
+              JOIN (employees)ON employees.PE_Nip=t3.KE_PE_NIPPengajar";
+
+        $order = "ORDER BY class_student.KU_KE_KR_MK_ID, class_student.KU_MA_Nrp";
+
+        $group = "group by class_student.KU_ID";
+
+        $filter = "WHERE t3.KE_Kelas=class_student.KU_KE_Kelas";
+
+        if (isset($filters['KE_Kelas'])) {
+            $filter = $filter . " " . " AND  t3.KE_Kelas=" . " '" . $filters['KE_Kelas'] . "'";
+        }
+        if (isset($filters['MA_Nrp'])) {
+            $filter = $filter . " " . "AND  students.MA_Nrp=" . " '" . $filters['MA_Nrp'] . "'";
+        }
+        if (isset($filters['MK_ID'])) {
+            $filter = $filter . " " . "AND  class_student.KU_KE_KR_MK_ID=" . " '" . $filters['MK_ID'] . "'";
+        }
+        if (isset($filters['min_percentage'])) {
+            $group = $group . " " . "having   persentase >=" . " '" . $filters['min_percentage'] . "'";
+        }
+        if (isset($filters['max_percentage'])) {
+            $group = $group . " " . "having  persentase <=" . " '" . $filters['max_percentage'] . "'";
+        }
+
+        if (isset($filters['equals'])) {
+            $group = $group . " " . "having  persentase =" . " '" . $filters['max_percentage'] . "'";
+        }
+
+
+        return DB::select($defaultQuery . " " . $filter . " " . $group." ".$order);
 
     }
 }
