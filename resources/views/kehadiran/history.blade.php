5cca8affc61630f8a019c08516d8b86a48d24fd7
CrytexEngineer <aqilmaulana1@gmail.com>
2020-05-11 03:45:09 +0800

    COMPLETE FLOW, REALTIME QR CODE

--- resources/views/kehadiran/history.blade.php
+++ resources/views/kehadiran/history.blade.php
@@ -1,40 +1,40 @@
 @extends('layouts.dosen')
 @section('title','Riwayat Pertemuan')
 @section('content')
-    @foreach($pertemuan as $row)
+
+    @include('alert')
+    @foreach($pertemuan  as $iterasiPertemuan)
     <br>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Riwayat Pertemuan ke-{{$row->index }} Perkuliahan</div>
+                    <div class="card-header">Riwayat Pertemuan ke-{{$loop->iteration }} Perkuliahan</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+
 
                         {{ Form::open(['url'=>'kehadiran/'.Request::segment(2)])}}
 
                         <table class="table table-bordered">
                             <a href="/kehadiran/{{ Request::segment(2)}}" class="btn btn-danger"><i class="fas fa-backward"></i> Kembali</a>
                             <hr>
-                            <tr><td width="270">Kode Matakuliah</td><td>{{ $jadwal->MK_ID}}</td></tr>
-                            <tr><td>Nama Matakuliah</td><td>{{ $jadwal->MK_Mata_Kuliah}}</td></tr>
-                            <tr><td>Kelas</td><td>{{ $jadwal->KE_Kelas}}</td></tr>
-                            <tr><td>Nama Dosen</td><td>{{ $jadwal->PE_NamaLengkap}}</td></tr>
+{{--                            <tr><td width="270">Kode Matakuliah</td><td>{{ $jadwal->MK_ID}}</td></tr>--}}
+{{--                            <tr><td>Nama Matakuliah</td><td>{{ $jadwal->MK_Mata_Kuliah}}</td></tr>--}}
+{{--                            <tr><td>Kelas</td><td>{{ $jadwal->KE_Kelas}}</td></tr>--}}
+{{--                            <tr><td>Nama Dosen</td><td>{{ $jadwal->PE_NamaLengkap}}</td></tr>--}}
                             <tr><td>Pertemuan Ke</td>
                                 <td>
-                                    {{ $pertemuan_ke }} {{ Form::hidden('pertemuan_ke',$pertemuan_ke )}}
+                                    {{ $loop->iteration }} {{ Form::hidden('pertemuan_ke',$loop->iteration )}}
                                 </td></tr>
-                            <tr><td>Jenis Kelas</td><td>{{ $pertemuan->PT_Type }}</td></tr>
-                            <tr><td>Topik</td><td>{{ $pertemuan->PT_Name}}</td></tr>
-                            <tr><td>Catatan</td><td>{{ $pertemuan->PT_Notes }}</td></tr>
+                            <tr><td>Hari</td><td>{{ date_format($iterasiPertemuan->created_at,'D, d-m-yy ') }}</td></tr>
+                            <tr><td>Waktu</td><td>{{ date_format($iterasiPertemuan->created_at,'H:i:s ') }}</td></tr>
+                            <tr><td>Jenis Kelas</td><td>{{ $iterasiPertemuan->PT_Type }}</td></tr>
+                            <tr><td>Topik</td><td>{{ $iterasiPertemuan->PT_Name}}</td></tr>
+                            <tr><td>Catatan</td><td>{{ $iterasiPertemuan->PT_Notes }}</td></tr>
                             <tr><td></td>
                                 <td>
-                                    <a href="#" class="btn btn-danger"><i class="fas fa-qrcode"></i> Show QR Code</a>
+                                    <a href='/validator/{{$iterasiPertemuan->PT_ID}}' class="btn btn-danger"><i class="fas fa-qrcode"></i> Show QR Code</a>
                                 </td>
                             </tr>
                         </table>
