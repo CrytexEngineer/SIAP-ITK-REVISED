27819119a6dd0a8c025950fe8496a2584f15905d
3pointcurve_ <azambinabdullah@gmail.com>
2020-05-27 00:58:23 +0800

    Fixing Conflict on ManajemenKurikulumController

--- app/Http/Controllers/ManajemenKurikulumController.php
+++ app/Http/Controllers/ManajemenKurikulumController.php
@@ -10,7 +10,11 @@ use Yajra\DataTables\DataTables;
 
 class ManajemenKurikulumController extends Controller
 {
-
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     function json()
     {
         return Datatables::of(Curiculum::all())
