0e234a3dd898c404cff5f6dbd75c1cf0c31f903f
3pointcurve_ <azambinabdullah@gmail.com>
2020-06-30 20:45:59 +0800

    Fix Input-Edit & Added Validations on Kelas & KHS

--- resources/views/khs/edit.blade.php
+++ resources/views/khs/edit.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.app')
-@section('title','Edit Data kelas')
+@section('title','Edit Data KHS')
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Edit Data Kelas</div>
+                    <div class="card-header">Edit Data KHS</div>
 
                     <div class="card-body">
                         @include('validation_error')
 
-                        {{ Form::model($kelas,['url'=>'kelas/'.$kelas->KE_ID,'method'=>'PUT'])}}
+                        {{ Form::model($khs,['url'=>'khs/'.$khs->KU_ID,'method'=>'PUT'])}}
 
                         @csrf
 
-                        @include('kelas.form')
+                        @include('khs.form')
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-3">
 
                                 {{ Form::submit('Simpan Data',['class'=>'btn btn-primary'])}}
-                                <a href="/kelas" class="btn btn-primary">Kembali</a>
+                                <a href="/khs" class="btn btn-primary">Kembali</a>
                             </div>
                         </div>
 
