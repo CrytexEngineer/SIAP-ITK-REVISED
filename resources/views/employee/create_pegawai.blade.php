53ac4dfab67f66f90d97fe7d7649125d185250ce
3pointcurve_ <azambinabdullah@gmail.com>
2020-07-01 23:08:35 +0800

    Update create_pegawai.blade.php

--- resources/views/employee/create_pegawai.blade.php
+++ resources/views/employee/create_pegawai.blade.php
@@ -50,14 +50,14 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Konfirmasi Password') }}</label>
+                            <label for="password-confirm" class="col-md-3 col-form-label text-md-right">{{ __('Konfirmasi Password') }}</label>
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Konfirmasi Password">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="roles" class="col-md-4 col-form-label text-md-right">Roles</label>
+                            <label for="roles" class="col-md-3 col-form-label text-md-right">Roles</label>
                             <div class="col-md-6">
                                 @foreach($roles as $role)
                                     <div class="form-check">
