e272a6e5309d0044107d18dc1ea994d8d7aa5de7
CrytexEngineer <aqilmaulana1@gmail.com>
2020-07-05 10:37:16 +0800

    Fix Bug Helper

--- app/Helper.php
+++ app/Helper.php
@@ -1,17 +1,31 @@
 <?php
 
 
-use App\Presence;
+//function getKehadiran($MA_Nrp, $KE_ID, $PT_URUTAN){
 
-function getKehadiran($MA_Nrp, $KE_ID, $PT_URUTAN){
+//    $presences=   Presence::JOIN('meetings','presences.PR_PT_ID','meetings.PT_ID')
+//        ->JOIN('class_student','class_student.KU_ID','presences.PR_KU_ID')
+//            ->WHERE('class_student.KU_MA_Nrp',$MA_Nrp)
+//            ->WHERE('presences.PR_KE_ID',$KE_ID)
+//            ->WHERE('meetings.PT_Urutan',3)->get()->first();
+//
+//
+//    if ($presences != null){
+//        return substr($presences['PR_Keterangan'],0,1);
+//    }
+//}
 
-    $presences=   Presence::JOIN('meetings','presences.PR_KE_ID','meetings.PT_KE_ID')
-        ->JOIN('class_student','class_student.KU_ID','presences.PR_KU_ID')
-            ->WHERE('class_student.KU_MA_Nrp',$MA_Nrp)
-            ->WHERE('presences.PR_KE_ID',$KE_ID)
-            ->WHERE('meetings.PT_Urutan',$PT_URUTAN)->get()->first();
+function getKehadiran($MA_Nrp, $KE_ID, $PT_URUTAN)
+{
 
-    if ($presences != null){
-        return substr($presences['PR_Keterangan'],0,1);
+    $presences = DB::select("Select * from presences  inner join
+    class_student on class_student.KU_ID = presences.PR_KU_ID
+    inner join meetings on presences.PR_PT_ID= meetings.PT_ID
+    where class_student.KU_MA_Nrp=" . $MA_Nrp . " and  presences.PR_KE_ID=" . $KE_ID . " and meetings.PT_Urutan=" . $PT_URUTAN);
+
+
+    if ($presences != null) {
+        return substr($presences[0]->{'PR_Keterangan'}, 0, 1);
     }
+
 }
